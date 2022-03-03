2ba5112bd2018d1e136a096c01da4651cc90048b
duncan <duncan.mcelfresh@gmail.com>
2022-03-03 09:25:17 -0800

    update test scripts

--- RecSys2019_DeepLearning_Evaluation/scripts/example_local_script.sh
+++ RecSys2019_DeepLearning_Evaluation/scripts/example_local_script.sh
@@ -7,6 +7,6 @@ source utils.sh
 # generate config files locally
 
 # run experiment
-test_gcp_commands ./example_config_2.txt dcm-test-script
+test_gcp_commands ./example_config_remote.txt dcm-test-script
 
 # pass
