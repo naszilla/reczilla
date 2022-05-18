658445389dab9694efe693952f7aeb63d9a2f684
crwhite14 <colin@realityengines.ai>
2022-05-18 08:02:04 +0000

    add new experiments

--- RecSys2019_DeepLearning_Evaluation/ReczillaClassifier/experiments/meta_perf_vs_num_algs.py
+++ RecSys2019_DeepLearning_Evaluation/ReczillaClassifier/experiments/meta_perf_vs_num_algs.py
@@ -20,13 +20,16 @@ from tqdm import tqdm
 
 ############### CONSTANTS ###############################
 METRIC = "test_metric_PRECISION_cut_10"
-LOGGER = get_logger("meta_perf_vs_num_train_datasets")
+LOGGER = get_logger("meta_perf_vs_num_algs")
 ALL_DATASETS = get_all_datasets()
 FIXED_ALGS_FEATS = True
 NUM_TRIALS = 5
 JUMP = 2
-NUM_ALGS = 10
 NUM_FEATS = 10
+NUM_TRAIN = 10
+
+# from initial experiments, it looked like the trends happened around 3-4, so I did this:
+NUM_ALGS_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 10, 13, 18]
 
 # test_dataset_family = "Movielens"
 # test_datasets = list(dataset_family_map.get(test_dataset_family, (test_dataset_family, )))
@@ -46,29 +49,29 @@ for test_dataset_family in tqdm(ALL_DATASET_FAMILIES):
         train_dataset_families = [d for d in ALL_DATASET_FAMILIES if d != test_dataset_family]
         random.shuffle(train_dataset_families)
         train_datasets = []
+        for tdf in train_dataset_families:
+            train_datasets += list(dataset_family_map.get(tdf, (tdf, )))
 
-        for num_train in range(JUMP, 20, JUMP):
-            for tdf in train_dataset_families[num_train - JUMP:num_train]:
-                train_datasets += list(dataset_family_map.get(tdf, (tdf, )))
+        for num_algs in NUM_ALGS_LIST:
 
             X_train, y_train, X_test, y_test, y_range_test = get_cached_featurized(
                 METRIC, test_datasets, METADATASET_NAME, cached_featurized, train_datasets,
-                fixed_algs_feats=FIXED_ALGS_FEATS, num_algs=NUM_ALGS, num_feats=NUM_FEATS)
+                fixed_algs_feats=FIXED_ALGS_FEATS, num_algs=num_algs, num_feats=NUM_FEATS)
 
             for ml in META_LEARNERS:
                 preds = run_metalearner(ml, X_train, y_train, X_test)
                 metrics = get_metrics(y_test, y_range_test, preds)
                 all_metrics[ml].append(metrics)
             
-            all_metrics['num_train'].append(num_train)
-            all_metrics['num_algs'].append(NUM_ALGS)
+            all_metrics['num_train'].append(NUM_TRAIN)
+            all_metrics['num_algs'].append(num_algs)
             all_metrics['num_feats'].append(NUM_FEATS)
 
         final_metrics[test_dataset_family][exp_id] = all_metrics
         print_special(f"{test_dataset_family} \n {all_metrics}", LOGGER)
 
 if FIXED_ALGS_FEATS:
-    file_name = "meta_perf_vs_num_train_fixed.pkl"
+    file_name = "meta_perf_vs_num_algs_fixed.pkl"
 else:
-    file_name = "meta_perf_vs_num_train.pkl"
+    file_name = "meta_perf_vs_num_algs.pkl"
 pickle.dump(final_metrics, open(f"ReczillaClassifier/results/"+file_name, "wb"))
--- RecSys2019_DeepLearning_Evaluation/ReczillaClassifier/experiments/meta_perf_vs_num_feats.py
+++ RecSys2019_DeepLearning_Evaluation/ReczillaClassifier/experiments/meta_perf_vs_num_feats.py
@@ -20,13 +20,17 @@ from tqdm import tqdm
 
 ############### CONSTANTS ###############################
 METRIC = "test_metric_PRECISION_cut_10"
-LOGGER = get_logger("meta_perf_vs_num_train_datasets")
+LOGGER = get_logger("meta_perf_vs_num_feats")
 ALL_DATASETS = get_all_datasets()
 FIXED_ALGS_FEATS = True
 NUM_TRIALS = 5
 JUMP = 2
 NUM_ALGS = 10
-NUM_FEATS = 10
+NUM_TRAIN = 10
+NUM_FEATS_LIST = [1, 2, 3, 6, 10, 19, 35, 64, 117, 211, 382]
+# computed using 
+# [int(j) for j in np.logspace(start=0.9, stop=np.log(382)/np.log(2), num=10, endpoint=True, base=2.0)]
+# and then adding "2"
 
 # test_dataset_family = "Movielens"
 # test_datasets = list(dataset_family_map.get(test_dataset_family, (test_dataset_family, )))
@@ -46,29 +50,29 @@ for test_dataset_family in tqdm(ALL_DATASET_FAMILIES):
         train_dataset_families = [d for d in ALL_DATASET_FAMILIES if d != test_dataset_family]
         random.shuffle(train_dataset_families)
         train_datasets = []
+        for tdf in train_dataset_families:
+            train_datasets += list(dataset_family_map.get(tdf, (tdf, )))
 
-        for num_train in range(JUMP, 20, JUMP):
-            for tdf in train_dataset_families[num_train - JUMP:num_train]:
-                train_datasets += list(dataset_family_map.get(tdf, (tdf, )))
+        for num_feats in NUM_FEATS_LIST:
 
             X_train, y_train, X_test, y_test, y_range_test = get_cached_featurized(
                 METRIC, test_datasets, METADATASET_NAME, cached_featurized, train_datasets,
-                fixed_algs_feats=FIXED_ALGS_FEATS, num_algs=NUM_ALGS, num_feats=NUM_FEATS)
+                fixed_algs_feats=FIXED_ALGS_FEATS, num_algs=NUM_ALGS, num_feats=num_feats)
 
             for ml in META_LEARNERS:
                 preds = run_metalearner(ml, X_train, y_train, X_test)
                 metrics = get_metrics(y_test, y_range_test, preds)
                 all_metrics[ml].append(metrics)
             
-            all_metrics['num_train'].append(num_train)
+            all_metrics['num_train'].append(NUM_TRAIN)
             all_metrics['num_algs'].append(NUM_ALGS)
-            all_metrics['num_feats'].append(NUM_FEATS)
+            all_metrics['num_feats'].append(num_feats)
 
         final_metrics[test_dataset_family][exp_id] = all_metrics
         print_special(f"{test_dataset_family} \n {all_metrics}", LOGGER)
 
 if FIXED_ALGS_FEATS:
-    file_name = "meta_perf_vs_num_train_fixed.pkl"
+    file_name = "meta_perf_vs_num_feats_fixed.pkl"
 else:
-    file_name = "meta_perf_vs_num_train.pkl"
+    file_name = "meta_perf_vs_num_feats.pkl"
 pickle.dump(final_metrics, open(f"ReczillaClassifier/results/"+file_name, "wb"))
